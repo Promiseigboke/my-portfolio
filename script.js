423f915d265bffff1b99fb97ada00990fc449efe
Promiseigboke <igbokesomtochukwupromise@gmail.com>
2023-02-24 16:41:46 +0100

    I finished the roman numeral section. I added styles to it. I changed the id of my showcontent function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button , toggle between hiding and showing the dropdown content */
 function showcontent() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("palindromeDropdown").classList.toggle("show");
 }
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
