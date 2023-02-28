fe136441fb29711f6ebb08a79371211497f8f0ba
Promiseigboke <igbokesomtochukwupromise@gmail.com>
2023-02-28 17:15:13 +0100

    I rectified the document.getElementById discovered another problem and rectified it . I also added min-width and max-width to make it better

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 /* When the user clicks on the button , toggle between hiding and showing the dropdown content */
-function showcontent() {
-    document.getElementById("palindromeDropdown").classList.toggle("show");
+function showcontent1() {
+    document.getElementById("romanDropdown").classList.toggle("show");
 }
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
+ function showcontent2() {
+    document.getElementById("palindromeDropdown").classList.toggle("show");
+} 
+// Close the dropdown if the user clicks outside of it 
+/* window.onclick = function(event) {
     if(!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
     }
-}
+} 
+*/
